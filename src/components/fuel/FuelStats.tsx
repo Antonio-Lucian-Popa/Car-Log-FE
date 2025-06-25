1da2f2e67cf9969dd1c04c549057ab71fdb8f9c2
antonio <antonio.popa.dev@gmail.com>
2025-06-25 11:54:02 +0300

    Update

--- src/components/fuel/FuelStats.tsx
+++ src/components/fuel/FuelStats.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { 
   Fuel, 
